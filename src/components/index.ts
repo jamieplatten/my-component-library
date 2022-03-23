3d18f581ccd9e42f1a083ffe6a21d4a860e359b4
Jamie Platten <jamie.platten@uea.ac.uk>
2022-03-23 08:28:29 +0000

    my component library initial upload

---
+++ src/components/index.ts
@@ -0,0 +1,43 @@
+export { Accordion } from "./Accordion";
+
+export { Button } from "./Button";
+
+export { Card } from "./Card";
+
+export { DescriptionList } from "./Lists/DescriptionList";
+
+export { OrderedList } from "./Lists/OrderedList";
+
+export { UnorderedList } from "./Lists/UnorderedList";
+
+export { CTA } from "./CTAs/CTA";
+
+export { CTAWithImage } from "./CTAs/CTAWithImage";
+
+export { CTACard } from "./CTAs/CTACard";
+
+export { BlockQuote } from "./BlockQuote";
+
+export { Carousel } from "./Carousel";
+
+export { default as FacebookSVG } from "./Facebook";
+
+export { Header } from "./Header";
+
+export { Hero } from "./Hero";
+
+export { Input } from "./Input";
+
+export { Instagram } from "./Instagram";
+
+export { Link } from "./Link";
+
+export { UEALogo } from "./UEALogo";
+
+export { Select } from "./Select";
+
+export { SidebarNav } from "./SidebarNav";
+
+export { Textarea } from "./Textarea";
+
+export { default as TwitterSVG } from "./Twitter";
